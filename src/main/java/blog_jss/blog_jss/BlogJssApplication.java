ee92de37017ffa56c980446a25fd323d1a3e829b
teenagemess <raynanda.aqiyas.ft21@mail.umy.ac.id>
2024-05-01 14:22:36 +0700

    first commit

---
+++ src/main/java/blog_jss/blog_jss/BlogJssApplication.java
@@ -0,0 +1,13 @@
+package blog_jss.blog_jss;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogJssApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogJssApplication.class, args);
+	}
+
+}
---
+++ src/test/java/blog_jss/blog_jss/BlogJssApplicationTests.java
@@ -0,0 +1,13 @@
+package blog_jss.blog_jss;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogJssApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
